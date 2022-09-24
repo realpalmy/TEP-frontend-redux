debd0029b10c2c1531d8b9a48e2deabf4424ff06
nicha.satt <nicha.satt@gmail.com>
2022-09-24 15:08:45 +0700

    Product Categoty Component

--- src/app/product_data.js
+++ src/app/product_data.js
@@ -3,28 +3,28 @@ const products = [
         id: 0,
         imgUrl: "https://play-lh.googleusercontent.com/9pTMs7YPMB5p0QMbhY0OH5niwmTT55anAVhu37ud3-O2u1Je06cxZY3mvgVhymieR9rJ",
         title: "UniqueCar1",
-        currentBid: 0,
-        buyNow: 0,
-        bitAmount: 0,
-        countDown: 0 
+        currentBid: 752,
+        buyNow: 896,
+        bitAmount: 2,
+        countDown: "00d:00h:00m:00s"
     },
     {
         id: 1,
         imgUrl: "https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/listicle/section/2021-Mercedes-AMG-GT-Black-Series-coupe-silver-1001x565-1.jpg",
         title: "UniqueCar2",
-        currentBid: 0,
-        buyNow: 0,
-        bitAmount: 0,
-        countDown: 0 
+        currentBid: 123,
+        buyNow: 456,
+        bitAmount: 22,
+        countDown: "00d:00h:00m:00s" 
     },
     {
         id: 2,
         imgUrl: "https://mpics.mgronline.com/pics/Images/565000007026101.JPEG",
         title: "UniqueCar2",
-        currentBid: 0,
-        buyNow: 0,
-        bitAmount: 0,
-        countDown: 0 
+        currentBid: 777,
+        buyNow: 999,
+        bitAmount: 30,
+        countDown: "00d:00h:00m:00s" 
     }
 ]
 
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,54 +1,62 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import "./test.css";
 import { GiTakeMyMoney } from "react-icons/gi";
-import { MdAttachMoney } from "react-icons/md";
+import { MdAttachMoney } from "react-icons/md";   
 
 
-function ProductCard({ product }) {
+function ProductCard( {product} ) {
     return (
-        <Fragment>
-            <div class="card" style={{ width: "18rem" }}>
-                <div class="col">
-                    <div class="card" style={{ width: "18rem" }}>
-                        <div class="card-body" style={{ height: "17rem" }}>
-                            <img className='rounded float-center img-fluid' style={{ "max-height": "18rem" }} src={product.imgUrl} alt="..." />
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">{product.title}</h5>
-                        </div>
-                        <ul class="list-group list-group-flush container">
-                            <div class="row">
-                                <div class="col-2 d-flex align-items-center">
-                                    <div class="px-1 text-success"><GiTakeMyMoney size={25} /></div>
-                                </div>
-                                <div class="col-4 border-end">
-                                    <div class="row">
-                                        <div class="text-10 text-success">CurrentBid</div>
-                                        <div>$999</div>
-                                    </div>
-                                </div>
-                                <div class="col-2 d-flex align-items-center">
-                                    <div class="px-1 text-danger"><MdAttachMoney size={25} /></div>
-                                </div>
-                                <div class="col-4">
-                                    <div class="row">
-                                        <div class="text-10 text-danger">Buynow</div>
-                                        <div>$855</div>
-                                    </div>
+        <>
+        <div class="col">
+            <div class="shadow card" style={{width: "18rem"}}>
+                <div class="card-body" style={{height: "17rem"}}>
+                    <img className='rounded float-center img-fluid'style={{"max-height": "18rem"}} src={product.imgUrl} alt="..." />
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">{product.title}</h5>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        <div class="row">
+                            <div class="col-2 d-flex align-items-center">
+                                <div class="text-success"><GiTakeMyMoney size={25}/></div>
+                            </div>
+                            <div class="col-4 border-end">
+                                <div class="row">
+                                    <div class="text-10 text-success">CurrentBid</div>
+                                    <div>${product.currentBid}</div>
                                 </div>
                             </div>
-                        </ul>
-
-                        <div class="card-body d-grid gap-2 col-lg-8 mx-auto text-center">
-                            <button class="btn btn-primary rounded-pill bg-gradient" type="button">
-                                <a href={"./"} class="text-decoration-none text-light">Submit A Bid</a>
-                            </button>
+                            <div class="col-2 d-flex align-items-center">
+                                <div class="text-danger"><MdAttachMoney size={25}/></div>
+                            </div>
+                            <div class="col-4">
+                                <div class="row">
+                                    <div class="text-10 text-danger">Buynow</div>
+                                    <div>${product.buyNow}</div>
+                                </div>
+                            </div>
                         </div>
+                    </li>
+                    
+                    <li class="list-group-item">
+                        <div className="row">
+                            <div class="col-8 border-end text-center text-danger">{product.countDown}</div>
+                            <div class="col-4 text-center text-success">{product.bitAmount} bid(s)</div>
+                        </div>
+                    </li>
 
-                    </div>
+                </ul>
+
+                <div class="card-body d-grid gap-2 col-lg-8 mx-auto text-center">
+                    <button class="btn btn-primary rounded-pill bg-gradient" type="button">
+                        <a href={"./"} class="text-decoration-none text-light">Submit A Bid</a>
+                    </button>
                 </div>
+                
             </div>
-        </Fragment>
+        </div>  
+        </>
     );
 }
 
