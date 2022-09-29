c17c6391333cdf61c4e39ce9c82172de561049ac
Chanakarn <chanakarn_h@cmu.ac.th>
2022-09-29 15:14:54 +0700

    Detail Product

--- src/components/DetailFrom.js
+++ src/components/DetailFrom.js
@@ -3,36 +3,34 @@ import styled from 'styled-components';
 
 export function DetailProduct({ className }) {
     return (
-        <div class="margin-top-25rem">
-            <div className={className}>
-                <div class="d-flex justify-content-center col-12 row mt-3">
-                    <div id="card-box" class="card rounded-5" style={{ width: "55rem" }}>
+        <div class="margin-top-25rem ">
+            <div className={className} >
+                <div class="d-flex justify-content-md-center row col-12">
+                    <div id="card-box" class="card rounded-5 col-sm-12 col-md-10 col-lg-8" style={{ width: "55rem" }}>
                         <img src="./image/01.png" class="size-img-bid" alt="..." />
                     </div>
                 </div>
-
-                <div class="p-5">
-                    <div class="d-flex justify-content-center col-12 row ">
-                        <div id="card-box" class="card rounded-5 mt-2 mx-3" style={{ width: "10rem", height: "8rem" }}>
-                            <img src="./image/02.png" class="show-img-bid" alt="..." />
+                <div class="p-5 d-flex row col-12 justify-content-md-center mb-5 ">
+                        <div class="row justify-content-center col-sm-12 col-md-10 ">
+                            <div id="card-box" class="card rounded-5 m-2" style={{ width: "10rem", height: "8rem" }}>
+                                <img src="./image/02.png" class="show-img-bid" alt="..." />
+                            </div>
+                            <div id="card-box" class="card rounded-5 m-2" style={{ width: "10rem", height: "8rem" }}>
+                                <img src="./image/search-icon.png" class="show-img-bid" alt="..." />
+                            </div>
+                            <div id="card-box" class="card rounded-5 m-2" style={{ width: "10rem", height: "8rem" }}>
+                                <img src="./image/04.png" class="show-img-bid" alt="..." />
+                            </div>
+                            <div id="card-box" class="card rounded-5 m-2" style={{ width: "10rem", height: "8rem" }}>
+                                <img src="./image/05.png" class="show-img-bid" alt="..." />
+                            </div>
+                            <div id="card-box" class="card rounded-5 m-2" style={{ width: "10rem", height: "8rem" }}>
+                                <img src="./image/06.png" class="show-img-bid" alt="..." />
+                            </div>
                         </div>
-                        <div id="card-box" class="card rounded-5 mt-2 mx-3" style={{ width: "10rem", height: "8rem" }}>
-                            <img src="./image/search-icon.png" class="show-img-bid" alt="..." />
-                        </div>
-                        <div id="card-box" class="card rounded-5 mt-2 mx-3" style={{ width: "10rem", height: "8rem" }}>
-                            <img src="./image/04.png" class="show-img-bid" alt="..." />
-                        </div>
-                        <div id="card-box" class="card rounded-5 mt-2 mx-3" style={{ width: "10rem", height: "8rem" }}>
-                            <img src="./image/05.png" class="show-img-bid" alt="..." />
-                        </div>
-                        <div id="card-box" class="card rounded-5 mt-2 mx-3" style={{ width: "10rem", height: "8rem" }}>
-                            <img src="./image/06.png" class="show-img-bid" alt="..." />
-                        </div>
-                    </div>
                 </div>
-
-                <div class=" row mt-5">
-                    <div class=" detail col-lg-6">
+                <div class=" d-flex p-5 row col-12 justify-content-md-center">
+                    <div class=" detail col-lg-6 col-md-8 col-sm-12 ">
                         <div class="product-content">
                             <div class="product-header" >
                                 <h1 class="">Product Name</h1>
@@ -44,27 +42,31 @@ export function DetailProduct({ className }) {
                                 <div class="product-detail d-flex " style={{ display: 'flex' }}>
                                     <h5>Current Price</h5>
                                     <h2 class="text-danger">$123.45</h2>
-
                                 </div>
                                 <div class="product-detail d-flex " style={{ display: 'flex' }}>
                                     <h5>Bid Increment (US) </h5>
                                     <h2>$50.00</h2>
-
                                 </div>
                             </ul>
-                            <div class="bid-area">
+                            <div class="bid-area mb-2">
                                 <form class="input-group rounded-pill ">
                                     <div class="search-icon">
                                         <img src="./image/search-icon.png" alt="..." style={{ width: "3rem", height: "3rem" }} />
                                     </div>
-                                    <input class="form-control rounded-pill p-2 mx-3 text-secondary" type="text" placeholder="Enter your bid amount..."></input>
+                                    <input class="form-control col-lg-6 col-md-6 col-sm-12 rounded-pill p-2 mx-3 text-secondary" type="text" placeholder="Enter your bid amount..."></input>
                                     <button type="button" class="btn-lg bg-4E598C rounded-pill p-2 text-white mx-3">Submit A Bid</button>
                                 </form>
                             </div>
+                            <div class="d-flex justify-content-center mt-3 mb-5">
+                                <button type="button" class="btn-lg bg-4E598C rounded-pill p-3 text-white px-3 mx-5">BUY NOW</button>
+                                <button type="button" class="btn-lg bg-watchlist rounded-pill p-3 text-dark px-3 mx-5">
+                                    <i class="bi bi-star "></i>
+                                    Add To WatchList
+                                </button>
+                            </div>
                         </div>
-
                     </div>
-                    <div class="show-time col-lg-3 align-self-center">
+                    <div class="mt-3 show-time col-lg-2 col-md-8 col-sm-12 ">
                         <h6 class="">This Auction Ends in:</h6>
                         <div class="show-count">
                             <div class="countdown">
@@ -73,9 +75,8 @@ export function DetailProduct({ className }) {
                         </div>
                     </div>
                 </div>
-
-                <div class="row mt-5">
-                    <div class="detail-content col-8">
+                <div class="row d-flex row col-12 justify-content-md-center mt-5 ">
+                    <div class="detail-content col-lg-8 col-md-10 col-sm-12">
                         <div class="item mb-5">
                             <h4 class="item-title mb-2">Detail 1: </h4>
                             <p class="mt-1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
@@ -88,9 +89,9 @@ export function DetailProduct({ className }) {
                             <h4 class="item-title mb-2">Detail 3: </h4>
                             <p class="mt-1">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
                         </div>
-
                     </div>
                 </div>
+
             </div>
         </div>
     );
@@ -114,10 +115,10 @@ ul li {
 
 }
 
-.detail,
-.detail-content {
-    margin-left: 12rem;
-}
+// .detail,
+// .detail-content {
+//     margin-left: 12rem;
+// }
 
 .product-detail {
     justify-content: space-between;
@@ -126,7 +127,7 @@ ul li {
 .show-price {
     background: #BBDEF0;
     border-radius: 20px;
-    padding: 30px 30px;
+    padding: 2rem 2rem;
     box-shadow: 0 3px 3px #8e8986cd;
 }
 
@@ -140,9 +141,9 @@ ul li {
     border: 1px solid rgba(255, 140, 66, 1);
     box-shadow: 0 3px 3px #8e8986cd;
     border-radius: 20px;
-    padding: 20px;
+    padding: 2rem 0 1rem 0;
     text-align: center;
-    margin-bottom: 100px;
+    margin-bottom: 10rem;
 }
 
 .countdown {
@@ -150,7 +151,7 @@ ul li {
 }
 
 #bid_counter1 {
-    font-size: 36px;
+    font-size: 24px;
     text-shadow: 0px 5px 3px rgba(82, 78, 79, 0.5);
 }
 
@@ -163,4 +164,8 @@ ul li {
 .detail {
     margin-top: -5rem;
 }
+.detail-content {
+    margin-top: -8rem;
+}
+
 `;
